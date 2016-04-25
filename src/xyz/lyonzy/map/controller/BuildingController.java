de46ccb62309e8a51fed75ca9fa0e52bd9c637a2
Brendan Lyons <brendan@codelyons.com>
2016-04-25 06:26:42 +0100

    Started creating the BuildingController.

---
+++ src/xyz/lyonzy/map/controller/BuildingController.java
@@ -0,0 +1,48 @@
+package xyz.lyonzy.map.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import xyz.lyonzy.map.model.Building;
+import xyz.lyonzy.map.model.Consts;
+import xyz.lyonzy.map.model.Database;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Brendan Lyons on 25/04/16.
+ */
+public class BuildingController implements Initializable{
+    @FXML
+    public ImageView buildingImageViw;
+    @FXML
+    Text buildingText, buildingName, openingHours;
+    @FXML
+    TableView roomTable;
+    @FXML
+    TableColumn roomsColums;
+    @FXML
+    Button close;
+
+    Database database = new Database();
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       try {
+           Building currentBuilding = database.getBuilding(Consts.getCurrentBuilding());
+           this.buildingName.setText(currentBuilding.getBuildingName());
+           this.buildingImageViw.setImage(new Image(currentBuilding.getImage()));
+           this.buildingText.setText(currentBuilding.getBuildingInfo());
+           this.openingHours.setText(currentBuilding.getOpeningHours());
+       }catch (Exception e){
+           System.out.println("Error");
+       }
+    }
+}
